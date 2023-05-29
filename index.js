84d8117e01101798ae3ae592b92a19ce93e86a01
Kirill <9255119632k@gmail.com>
2023-05-30 01:51:17 +0300

    .

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+  const rows = content.trim().split('\r\n');
+  const data = rows.map((row) => row.split(','))
+  console.log (`amount: ${data.length - 1}`)
   // END
 }
